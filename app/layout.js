f27c5150b51d5ea7d18eec77a931bdcd0bbe1389
walexy026 <jao.olaniyi@gmail.com>
2023-11-28 18:27:54 +0100

    about us2

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Navbar />
-        <Footer />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import About from "@/components/home/About";
-import Carousal from "@/components/home/Carousal";
+// import Carousal from "@/components/home/Carousal";
 import CoreTeam from "@/components/home/CoreTeam";
 import Donate from "@/components/home/Donate";
 import Hero from "@/components/home/Hero";
@@ -9,15 +9,14 @@ import Testimonials from "@/components/home/Testimonials";
 
 export default function Home() {
   return (
-    <h1>
-      hello is hard
+    <>
+      <Hero />
       <About />
       {/* <Carousal /> */}
       <CoreTeam />
-      {/* <Donate /> */}
-      {/* <Hero /> */}
-      {/* <Stats /> */}
-      {/* <Testimonials /> */}
-    </h1>
+      <Donate />
+      <Stats />
+      <Testimonials />
+    </>
   );
 }
